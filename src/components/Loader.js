0640e4c04a622508ef1e66c657efe5e7bd3cc50b
Mahfuj Ahmed Rony <105776505+Rony-code-hub@users.noreply.github.com>
2024-04-29 09:18:33 +0600

    Add files via upload

---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+function Loader() {
+  return (
+    <div className="loader-container">
+      <div class="loader"></div>
+      <p>Loading questions...</p>
+    </div>
+  );
+}
+
+export default Loader;
